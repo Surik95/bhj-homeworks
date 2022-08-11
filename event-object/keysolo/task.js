7e3910059f3fb1d48784955878eb5d6c84d7feb1
Surik95 <pokemon.surik@gmail.com>
2022-08-11 18:12:58 +0600

    Выполнил домашнее задание по теме 3

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,19 +4,34 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
-
+    this.timeElement = container.querySelector('.status__time')
+  
+    
     this.reset();
-
+    this.setNewWord(); 
     this.registerEvents();
   }
 
   reset() {
-    this.setNewWord();
     this.winsElement.textContent = 0;
-    this.lossElement.textContent = 0;
+    this.lossElement.textContent = 0; 
   }
 
   registerEvents() {
+    let exceptions = ['Shift', 'Alt']
+      
+    
+    window.addEventListener('keyup', (e) => {
+      if (!exceptions.includes(e.key)) {
+        if (e.key.toUpperCase() === this.currentSymbol.textContent.toUpperCase()){
+        this.success() 
+        }  else {
+          this.fail()
+        }
+    }})
+    
+    
+    
     /*
       TODO:
       Написать обработчик события, который откликается
@@ -34,23 +49,49 @@ class Game {
     }
 
     if (++this.winsElement.textContent === 10) {
-      alert('Победа!');
-      this.reset();
+      setTimeout(()=> {
+        alert('Победа!')
+        this.reset();
+      }, 200)
     }
+    this.clearCounter()
     this.setNewWord();
   }
 
   fail() {
+    this.clearCounter()
     if (++this.lossElement.textContent === 5) {
-      alert('Вы проиграли!');
-      this.reset();
+      setTimeout(()=> {
+        alert('Вы проиграли!')
+        this.reset();
+      }, 200)
     }
     this.setNewWord();
+    
   }
 
   setNewWord() {
     const word = this.getWord();
 
+    
+    let wordLength = word.length
+    this.timeElement.textContent = this.timeElement.textContent = `${wordLength} секунд`
+    this.timeInterval = setInterval(() => {
+        if (--wordLength === 1) {
+          this.timeElement.textContent = `${wordLength} секунда`
+        } else if  ((wordLength === 2) || (wordLength === 3)|| (wordLength === 4)) {
+          this.timeElement.textContent = `${wordLength} секунды`
+        } else {
+          this.timeElement.textContent = `${wordLength} секунд`
+        }
+      }, 1000);
+
+    let time = Number(`${word.length}000`)
+    this.timeout = setTimeout( (() => {
+      console.log(time,this.timeout);
+      this.fail()
+    }),time)
+    
     this.renderWord(word);
   }
 
@@ -58,15 +99,17 @@ class Game {
     const words = [
         'bob',
         'awesome',
-        'netology',
+        'я люблю netology',
         'hello',
         'kitty',
         'rock',
         'youtube',
-        'popcorn',
+        'я люблю popcorn',
         'cinema',
         'love',
-        'javascript'
+        'я люблю javascript',
+        'я люблю kitkat',
+
       ],
       index = Math.floor(Math.random() * words.length);
 
@@ -80,11 +123,19 @@ class Game {
           `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
       )
       .join('');
+    
     this.wordElement.innerHTML = html;
 
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
   }
+
+  clearCounter(){
+    this.timeElement.textContent = ''
+    clearInterval(this.timeInterval)
+    clearTimeout(this.timeout)
+  }
+
 }
 
-new Game(document.getElementById('game'))
 
+new Game(document.getElementById('game'))
